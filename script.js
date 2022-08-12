35036075456e5739c417526d503584c740ab65ba
chrizzy-ghold <akubernicetay@gmail.com>
2022-08-12 14:18:29 +0000

    MenuSlider

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggle =  ducument.getElementby('toggle');
+const close =  ducument.getElementby('close');
+const open =  ducument.getElementby('open');
+const modal = document.getElementById('modal');
+
+// add Event listenerto toggle button
+
+toggle.addEeventListener("click", ()=> {
+modal.classList.add('show-modal');
+})
+
+// add event listener to add button
+open.classList
