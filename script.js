89d68507e569c7258e637fa7427f9a1cc5f4adcf
chrizzy-ghold <akubernicetay@gmail.com>
2022-08-12 16:04:12 +0000

    Menu Slider

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const toggle =  ducument.getElementby('toggle');
-const close =  ducument.getElementby('close');
-const open =  ducument.getElementby('open');
-const modal = document.getElementById('modal');
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
 
-// add Event listenerto toggle button
 
-toggle.addEeventListener("click", ()=> {
-modal.classList.add('show-modal');
-})
+// add event listener to toggle button
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("show-nav");
+});
 
 // add event listener to add button
-open.classList
+open.addEventListener("click", () => {
+  modal.classList.add("show-modal");
+})
+
+// add event listener to close button
+close.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+})
+
+// close modal on window click
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.classList.remove("show-modal");
+  }
+})
